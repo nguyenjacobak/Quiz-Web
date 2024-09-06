f285b7c1e06a4f4c0653744a450d25b071602786
nguyenjacobak <nguyenjacobak@gmail.com>
2024-09-06 19:45:29 +0700

    quizweb

--- BackEnd/base/admin.py
+++ BackEnd/base/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Message, Blog
 
 # Register your models here.
-admin.site.register(Message)
-admin.site.register(Blog)
--- BackEnd/quiz/admin.py
+++ BackEnd/quiz/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from .models import Category, Quiz, Question, Choice, QuizSubmission, UserRank
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Quiz)
-admin.site.register(Question)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('question', 'text', 'is_correct')
-
-admin.site.register(Choice, ChoiceAdmin)
-admin.site.register(QuizSubmission)
-admin.site.register(UserRank)
